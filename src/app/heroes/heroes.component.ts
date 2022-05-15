b2412f0524bd1ec0c262d8b74ea956507b5ae55a
Ronaldo Victor <ronaldovctor@outlook.com>
2022-05-15 17:58:53 -0300

    Adding details hero input

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from '../data/mock-heroes';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,15 @@ import { HEROES } from '../data/mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES
+  selectedHero?: Hero
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
   }
 
 }
