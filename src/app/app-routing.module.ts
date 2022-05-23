6d59d932d09c9bb027268d02755d85cdab0c51a7
Ronaldo Victor <ronaldovctor@outlook.com>
2022-05-22 21:24:02 -0300

    Adding navigation by routes pt.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroesComponent } from './heroes/heroes.component';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: 'heroes', component: HeroesComponent
+  },
+  {
+    path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path: '', redirectTo: '/dashboard', pathMatch: 'full'
   }
 ]
 
