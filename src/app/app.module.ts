aa6836310d987263aa1432f866682ea997a608f6
Ronaldo Victor <ronaldovctor@outlook.com>
2022-05-29 20:32:35 -0300

    Getting data from a server pt.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HttpClientInMemoryWebApiModule, InMemoryDbService, InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
